ef08b4527e16ca34f4dd4232629d783e0dcc33a3
Henry Janke <henry.j@aluno.ifsc.edu.br>
2023-04-13 08:54:39 -0300

    Revert "desing"
    This reverts commit d8718ef137522a8c4b68ea714e2b18340b72449f.

--- Blaze/src/visao/janela.java
+++ Blaze/src/visao/janela.java
@@ -1,6 +1,5 @@
 package visao;
 
-import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 
@@ -52,13 +51,9 @@ public class janela extends JFrame {
 	 * Create the frame.
 	 */
 	public janela() {
-		
-	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		setBounds(100, 100, 1473, 899);
+		setBounds(100, 100, 1197, 726);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(125,0,	0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -67,31 +62,21 @@ public class janela extends JFrame {
 		dao = UsuarioDAO.getFUsuarioDAO();
 		
 		JLabel lblNome = new JLabel("Nome: ");
-		lblNome.setForeground(new Color(255, 255, 255));
-		lblNome.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNome.setBounds(89, 367, 50, 14);
+		lblNome.setBounds(82, 224, 46, 14);
 		contentPane.add(lblNome);
 		
 		JLabel lblEmail = new JLabel("Email: ");
-		lblEmail.setForeground(new Color(255, 255, 255));
-		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblEmail.setBounds(89, 415, 50, 14);
+		lblEmail.setBounds(82, 272, 31, 14);
 		contentPane.add(lblEmail);
 		
 		JLabel lblCpf = new JLabel("Cpf: ");
-		lblCpf.setForeground(new Color(255, 255, 255));
-		lblCpf.setBackground(new Color(255, 255, 255));
-		lblCpf.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblCpf.setBounds(101, 466, 34, 14);
+		lblCpf.setBounds(82, 323, 46, 14);
 		contentPane.add(lblCpf);
 		
 		JLabel lblTelefone = new JLabel("Telefone: ");
-		lblTelefone.setForeground(new Color(255, 255, 255));
-		lblTelefone.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblTelefone.setBounds(65, 508, 77, 14);
+		lblTelefone.setBounds(82, 365, 77, 14);
 		contentPane.add(lblTelefone);
 		
-<<<<<<< Updated upstream
 		txtNome = new JTextField();
 		txtNome.setBounds(138, 221, 412, 20);
 		contentPane.add(txtNome);
@@ -123,39 +108,11 @@ public class janela extends JFrame {
 			}
 		});
 		btnNewButton.setBounds(234, 461, 227, 23);
-=======
-		textNome = new JTextField();
-		textNome.setBounds(200, 367, 412, 20);
-		contentPane.add(textNome);
-		textNome.setColumns(10);
-		
-		textEmail = new JTextField();
-		textEmail.setColumns(10);
-		textEmail.setBounds(200, 415, 412, 20);
-		contentPane.add(textEmail);
-		
-		textCpf = new JTextField();
-		textCpf.setColumns(10);
-		textCpf.setBounds(200, 466, 412, 20);
-		contentPane.add(textCpf);
-		
-		textTelefone = new JTextField();
-		textTelefone.setColumns(10);
-		textTelefone.setBounds(200, 508, 412, 20);
-		contentPane.add(textTelefone);
-		
-		JButton btnNewButton = new JButton("Cadastrar");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnNewButton.setBackground(new Color(153,203,153));
-		btnNewButton.setForeground(new Color(0, 0, 0));
-		btnNewButton.setBounds(273, 697, 281, 54);
->>>>>>> Stashed changes
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel = new JLabel("Tela De Cadastro");
-		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 26));
-		lblNewLabel.setBounds(22, 11, 343, 70);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(22, 11, 176, 70);
 		contentPane.add(lblNewLabel);
 		
 		// Adicionando o JTable
@@ -170,7 +127,7 @@ public class janela extends JFrame {
 		
 		// Adicionando o JTable em um JScrollPane
 		JScrollPane scrollPane = new JScrollPane(tabelaUsuarios);
-		scrollPane.setBounds(801, 341, 687, 496);
+		scrollPane.setBounds(600, 100, 550, 500);
 		contentPane.add(scrollPane);
 	}
 }
