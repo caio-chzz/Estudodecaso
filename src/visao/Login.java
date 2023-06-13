cb8c3506c3b78691a4bd591503465ec0c47c4cdc
Henry Janke <henry.j@aluno.ifsc.edu.br>
2023-06-12 21:01:01 -0600

    ainda deixando menos feio

--- src/visao/Login.java
+++ src/visao/Login.java
@@ -15,6 +15,8 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.text.MaskFormatter;
 import javax.swing.ImageIcon;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
 public class Login extends JFrame {
 
@@ -22,9 +24,9 @@ public class Login extends JFrame {
 	private JTextField textSenha;
 	private JTextField textEmail;
 	private JLabel lblBemVindo;
-	private JLabel lblNoPossuiCadastro;
 	private JButton btnCadastrese;
 	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_2;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -47,9 +49,10 @@ public class Login extends JFrame {
 	public Login() {
 		setTitle("Tela inicial");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1303, 718);
+		setBounds(100, 100, 1303, 781);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(128, 0, 255));
+		contentPane.setForeground(Color.PINK);
+		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -58,45 +61,39 @@ public class Login extends JFrame {
 		JLabel lblSenha = new JLabel("Senha:");
 		lblSenha.setFont(new Font("Tahoma", Font.BOLD, 21));
 		lblSenha.setForeground(Color.WHITE);
-		lblSenha.setBounds(839, 412, 103, 60);
+		lblSenha.setBounds(193, 524, 103, 60);
 		contentPane.add(lblSenha);
 
 		JLabel lblEmail = new JLabel("Email:");
 		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 21));
 		lblEmail.setForeground(Color.WHITE);
-		lblEmail.setBounds(839, 291, 70, 60);
+		lblEmail.setBounds(193, 398, 70, 60);
 		contentPane.add(lblEmail);
 
 		textSenha = new JTextField();
 		textSenha.setBackground(new Color(192, 192, 192));
-		textSenha.setBounds(997, 316, 208, 20);
+		textSenha.setBounds(306, 422, 225, 28);
 		contentPane.add(textSenha);
 		textSenha.setColumns(10);
 
 		textEmail = new JTextField();
 		textEmail.setBackground(new Color(192, 192, 192));
 		textEmail.setColumns(10);
-		textEmail.setBounds(997, 437, 208, 20);
+		textEmail.setBounds(306, 540, 225, 28);
 		contentPane.add(textEmail);
 
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setBackground(new Color(128, 128, 128));
 		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 21));
 		btnLogin.setForeground(Color.BLACK);
-		btnLogin.setBounds(839, 549, 145, 60);
+		btnLogin.setBounds(173, 671, 145, 60);
 		contentPane.add(btnLogin);
 
 		lblBemVindo = new JLabel("Bem Vindo!");
 		lblBemVindo.setForeground(Color.WHITE);
 		lblBemVindo.setFont(new Font("Tahoma", Font.BOLD, 40));
-		lblBemVindo.setBounds(868, 138, 409, 60);
+		lblBemVindo.setBounds(193, 187, 409, 60);
 		contentPane.add(lblBemVindo);
-
-		lblNoPossuiCadastro = new JLabel("Não possui cadastro?");
-		lblNoPossuiCadastro.setForeground(Color.WHITE);
-		lblNoPossuiCadastro.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNoPossuiCadastro.setBounds(1027, 495, 191, 60);
-		contentPane.add(lblNoPossuiCadastro);
 
 		btnCadastrese = new JButton("Cadastre-se");
 		btnCadastrese.addActionListener(new ActionListener() {
@@ -110,13 +107,18 @@ public class Login extends JFrame {
 		btnCadastrese.setBackground(new Color(128, 128, 128));
 		btnCadastrese.setForeground(Color.BLACK);
 		btnCadastrese.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		btnCadastrese.setBounds(1067, 549, 168, 60);
+		btnCadastrese.setBounds(363, 671, 168, 60);
 		contentPane.add(btnCadastrese);
 		
-		lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/imagens/viaofundo2.png")));
-		lblNewLabel.setBounds(-566, -27, 1288, 1220);
+		lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/imagens/Design_sem_nome__1_-removebg-preview.png")));
+		lblNewLabel.setBounds(144, 151, 1198, 619);
 		contentPane.add(lblNewLabel);
+		
+		lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon(Login.class.getResource("/imagens/Design_sem_nome__3_-removebg-preview.png")));
+		lblNewLabel_2.setBounds(1191, 68, 786, 516);
+		contentPane.add(lblNewLabel_2);
 
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
