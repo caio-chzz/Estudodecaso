f5eaee9de9253817e6038d824e714e589f0bd1be
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-06-17 11:19:43 -0300

    tel ON BIXO DO PAU

--- src/visao/Tela_EscAssento.java
+++ src/visao/Tela_EscAssento.java
@@ -11,6 +11,8 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Tela_EscAssento extends JFrame {
 
@@ -20,57 +22,103 @@ public class Tela_EscAssento extends JFrame {
 	
 	public Tela_EscAssento() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 811, 488);
+		setBounds(100, 100, 1147, 727);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(56, 124, 154));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblEsc = new JLabel("Escolha um assento: ");
-		lblEsc.setBounds(59, 24, 238, 14);
-		contentPane.add(lblEsc);
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(56, 124, 154));
+		panel.setBounds(408, 112, 949, 590);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
-		JRadioButton rdbtn01 = new JRadioButton("1");
-		rdbtn01.setBounds(109, 150, 109, 23);
-		contentPane.add(rdbtn01);
-		
-		JRadioButton rdbtn02 = new JRadioButton("2");
-		rdbtn02.setBounds(109, 193, 109, 23);
-		contentPane.add(rdbtn02);
-		
-		JRadioButton rdbtn03 = new JRadioButton("3");
-		rdbtn03.setBounds(237, 150, 109, 23);
-		contentPane.add(rdbtn03);
-		
-		JRadioButton rdbtn04 = new JRadioButton("4");
-		rdbtn04.setBounds(237, 193, 109, 23);
-		contentPane.add(rdbtn04);
-		
-		JRadioButton rdbtn05 = new JRadioButton("5");
-		rdbtn05.setBounds(363, 150, 109, 23);
-		contentPane.add(rdbtn05);
-		
-		JRadioButton rdbtn06 = new JRadioButton("6");
-		rdbtn06.setBounds(363, 193, 109, 23);
-		contentPane.add(rdbtn06);
-		
-		JRadioButton rdbtn07 = new JRadioButton("7");
-		rdbtn07.setBounds(490, 150, 109, 23);
-		contentPane.add(rdbtn07);
-		
-		JRadioButton rdbtn08 = new JRadioButton("8");
-		rdbtn08.setBounds(490, 193, 109, 23);
-		contentPane.add(rdbtn08);
-		
-		txtInfos = new JTextField();
-		txtInfos.setEnabled(false);
-		txtInfos.setText("Recebe as informações do voo escolhido\r\n");
-		txtInfos.setBounds(109, 282, 572, 20);
-		contentPane.add(txtInfos);
-		txtInfos.setColumns(10);
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.setBackground(new Color(255, 128, 128));
+		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnVoltar.setBounds(374, 459, 144, 44);
+		panel.add(btnVoltar);
 		
 		JButton btnSalvar = new JButton("Salvar\r\n");
+		btnSalvar.setBackground(new Color(0, 255, 128));
+		btnSalvar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnSalvar.setBounds(168, 457, 144, 44);
+		panel.add(btnSalvar);
+		
+		JRadioButton rdbtn08 = new JRadioButton("8");
+		rdbtn08.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn08.setBounds(490, 180, 109, 23);
+		panel.add(rdbtn08);
+		rdbtn08.setBackground(new Color(56, 124, 154));
+		
+		JRadioButton rdbtn07 = new JRadioButton("7");
+		rdbtn07.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn07.setBounds(490, 111, 109, 23);
+		panel.add(rdbtn07);
+		rdbtn07.setBackground(new Color(56, 124, 154));
+		
+		JRadioButton rdbtn05 = new JRadioButton("5");
+		rdbtn05.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn05.setBounds(342, 111, 109, 23);
+		panel.add(rdbtn05);
+		rdbtn05.setBackground(new Color(56, 124, 154));
+		
+		JRadioButton rdbtn06 = new JRadioButton("6");
+		rdbtn06.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn06.setBounds(342, 180, 109, 23);
+		panel.add(rdbtn06);
+		rdbtn06.setBackground(new Color(56, 124, 154));
+		
+		JRadioButton rdbtn03 = new JRadioButton("3");
+		rdbtn03.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn03.setBounds(203, 111, 109, 23);
+		panel.add(rdbtn03);
+		rdbtn03.setBackground(new Color(56, 124, 154));
+		
+		JRadioButton rdbtn04 = new JRadioButton("4");
+		rdbtn04.setBounds(203, 181, 109, 23);
+		panel.add(rdbtn04);
+		rdbtn04.setBackground(new Color(56, 124, 154));
+		
+		JRadioButton rdbtn01 = new JRadioButton("1");
+		rdbtn01.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn01.setBounds(68, 111, 109, 23);
+		panel.add(rdbtn01);
+		rdbtn01.setBackground(new Color(56, 124, 154));
+		
+		JRadioButton rdbtn02 = new JRadioButton("2");
+		rdbtn02.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn02.setBounds(68, 180, 109, 23);
+		panel.add(rdbtn02);
+		rdbtn02.setBackground(new Color(56, 124, 154));
+		
+		txtInfos = new JTextField();
+		txtInfos.setForeground(new Color(0, 0, 0));
+		txtInfos.setBounds(61, 307, 647, 36);
+		panel.add(txtInfos);
+		txtInfos.setEnabled(false);
+		txtInfos.setText("Recebe as informações do voo escolhido\r\n");
+		txtInfos.setColumns(10);
+		
+		JLabel lblEsc = new JLabel("Escolha um assento: ");
+		lblEsc.setFont(new Font("Tahoma", Font.BOLD, 31));
+		lblEsc.setBounds(50, 25, 425, 54);
+		panel.add(lblEsc);
+		
+		JRadioButton rdbtn07_1 = new JRadioButton("9");
+		rdbtn07_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn07_1.setBackground(new Color(56, 124, 154));
+		rdbtn07_1.setBounds(638, 113, 109, 23);
+		panel.add(rdbtn07_1);
+		
+		JRadioButton rdbtn07_2 = new JRadioButton("10");
+		rdbtn07_2.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtn07_2.setBackground(new Color(56, 124, 154));
+		rdbtn07_2.setBounds(638, 180, 109, 23);
+		panel.add(rdbtn07_2);
 		btnSalvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -79,10 +127,6 @@ public class Tela_EscAssento extends JFrame {
 				pagamento.setVisible(true);
 			}
 		});
-		btnSalvar.setBounds(203, 415, 89, 23);
-		contentPane.add(btnSalvar);
-		
-		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -91,24 +135,5 @@ public class Tela_EscAssento extends JFrame {
 				cadastrarPassageiro.setVisible(true);	
 			}
 		});
-		btnVoltar.setBounds(398, 415, 89, 23);
-		contentPane.add(btnVoltar);
-		
-		JButton btnSair = new JButton("Sair");
-		btnSair.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
-		btnSair.setBounds(696, 11, 89, 23);
-		contentPane.add(btnSair);
-		
-		JLabel lblNewLabel = new JLabel("assento ocupado");
-		lblNewLabel.setBounds(200, 342, 97, 14);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Assento livre");
-		lblNewLabel_1.setBounds(377, 342, 95, 14);
-		contentPane.add(lblNewLabel_1);
 	}
 }
--- src/visao/Tela_Pagamento.java
+++ src/visao/Tela_Pagamento.java
@@ -11,6 +11,8 @@ import javax.swing.JRadioButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Tela_Pagamento extends JFrame {
 
@@ -19,30 +21,79 @@ public class Tela_Pagamento extends JFrame {
 
 	public Tela_Pagamento() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1150, 612);
+		setBounds(100, 100, 1150, 746);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(56, 124, 154));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblFormasdPagamento = new JLabel("Formas de pagamento");
-		lblFormasdPagamento.setBounds(133, 59, 642, 14);
-		contentPane.add(lblFormasdPagamento);
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(56, 124, 154));
+		panel.setBounds(323, 141, 959, 542);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
-		JLabel lblimgPix = new JLabel("ImgPix");
-		lblimgPix.setBounds(182, 180, 111, 54);
-		contentPane.add(lblimgPix);
-		
-		JLabel lblImgcartao = new JLabel("ImgCartao");
-		lblImgcartao.setBounds(386, 180, 111, 54);
-		contentPane.add(lblImgcartao);
-		
-		JLabel lblImgdinheiro = new JLabel("ImgDinheiro");
-		lblImgdinheiro.setBounds(643, 180, 111, 54);
-		contentPane.add(lblImgdinheiro);
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.setBackground(new Color(255, 128, 128));
+		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnVoltar.setBounds(329, 432, 143, 45);
+		panel.add(btnVoltar);
 		
 		JButton btnFinalizar = new JButton("Finalizar");
+		btnFinalizar.setBackground(new Color(0, 255, 128));
+		btnFinalizar.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnFinalizar.setBounds(49, 432, 143, 45);
+		panel.add(btnFinalizar);
+		
+		JRadioButton rdbtnPix = new JRadioButton("Pix");
+		rdbtnPix.setBackground(new Color(56, 124, 154));
+		rdbtnPix.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtnPix.setBounds(97, 317, 109, 23);
+		panel.add(rdbtnPix);
+		
+		JRadioButton rdbtnCartao = new JRadioButton("Cartao");
+		rdbtnCartao.setBackground(new Color(56, 124, 154));
+		rdbtnCartao.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtnCartao.setBounds(312, 317, 109, 23);
+		panel.add(rdbtnCartao);
+		
+		JRadioButton rdbtnDinheiro = new JRadioButton("Dinheiro");
+		rdbtnDinheiro.setBackground(new Color(56, 124, 154));
+		rdbtnDinheiro.setFont(new Font("Tahoma", Font.BOLD, 13));
+		rdbtnDinheiro.setBounds(508, 317, 109, 23);
+		panel.add(rdbtnDinheiro);
+		
+		JLabel lblImgcartao = new JLabel("ImgCartao");
+		lblImgcartao.setBounds(312, 128, 111, 54);
+		panel.add(lblImgcartao);
+		
+		JLabel lblImgdinheiro = new JLabel("ImgDinheiro");
+		lblImgdinheiro.setBounds(520, 128, 111, 54);
+		panel.add(lblImgdinheiro);
+		
+		JLabel lblimgPix = new JLabel("ImgPix");
+		lblimgPix.setBounds(109, 128, 111, 54);
+		panel.add(lblimgPix);
+		
+		JLabel lblValorTotal = new JLabel("Valor total:");
+		lblValorTotal.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblValorTotal.setBounds(508, 10, 111, 54);
+		panel.add(lblValorTotal);
+		
+		txtRecebeValorFixo = new JTextField();
+		txtRecebeValorFixo.setBackground(new Color(56, 124, 154));
+		txtRecebeValorFixo.setBounds(616, 28, 261, 20);
+		panel.add(txtRecebeValorFixo);
+		txtRecebeValorFixo.setText("Recebe valor ");
+		txtRecebeValorFixo.setEnabled(false);
+		txtRecebeValorFixo.setColumns(10);
+		
+		JLabel lblFormasdPagamento = new JLabel("Formas de pagamento");
+		lblFormasdPagamento.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblFormasdPagamento.setBounds(31, 30, 362, 34);
+		panel.add(lblFormasdPagamento);
 		btnFinalizar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -51,31 +102,6 @@ public class Tela_Pagamento extends JFrame {
 				menu.setVisible(true);
 			}
 		});
-		btnFinalizar.setBounds(158, 446, 89, 23);
-		contentPane.add(btnFinalizar);
-		
-		JRadioButton rdbtnCartao = new JRadioButton("Cartao");
-		rdbtnCartao.setBounds(374, 300, 109, 23);
-		contentPane.add(rdbtnCartao);
-		
-		JRadioButton rdbtnDinheiro = new JRadioButton("Dinheiro");
-		rdbtnDinheiro.setBounds(602, 300, 109, 23);
-		contentPane.add(rdbtnDinheiro);
-		
-		JRadioButton rdbtnPix = new JRadioButton("Pix");
-		rdbtnPix.setBounds(184, 300, 109, 23);
-		contentPane.add(rdbtnPix);
-		
-		JButton btnSair = new JButton("Sair");
-		btnSair.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
-		btnSair.setBounds(1035, 11, 89, 23);
-		contentPane.add(btnSair);
-		
-		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -84,18 +110,5 @@ public class Tela_Pagamento extends JFrame {
 				escAssento.setVisible(true);
 			}
 		});
-		btnVoltar.setBounds(10, 11, 89, 23);
-		contentPane.add(btnVoltar);
-		
-		JLabel lblValorTotal = new JLabel("Valor total");
-		lblValorTotal.setBounds(643, 59, 111, 54);
-		contentPane.add(lblValorTotal);
-		
-		txtRecebeValorFixo = new JTextField();
-		txtRecebeValorFixo.setText("Recebe valor fixo");
-		txtRecebeValorFixo.setEnabled(false);
-		txtRecebeValorFixo.setColumns(10);
-		txtRecebeValorFixo.setBounds(707, 76, 261, 20);
-		contentPane.add(txtRecebeValorFixo);
 	}
 }
