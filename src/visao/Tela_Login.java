2ecae1da609ab37aca88d11234d7a37fbc9833fb
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-06-17 08:41:02 -0300

    ratinhooo

--- src/visao/Tela_Login.java
+++ src/visao/Tela_Login.java
@@ -17,6 +17,8 @@ import java.awt.event.ActionEvent;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Tela_Login extends JFrame {
 
@@ -45,43 +47,76 @@ public class Tela_Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Tela_Login() {
+		setBackground(new Color(255, 255, 255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 837, 437);
+		setBounds(100, 100, 1198, 740);
 		BufferedImage bg = null;
 		
-		try {
-			bg = ImageIO.read(new File("src/imagens/fundoSalvador.png"));
+		
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
+			
 
 		contentPane = new VBackGround(bg);
+		contentPane.setBackground(new Color(82, 159, 192));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		
-		JLabel lblEmail = new JLabel("Email");
-		
-		textEmail = new JTextField();
-		textEmail.setColumns(10);
-		
-		JLabel lblSenha = new JLabel("Senha");
-		
-		textSenha = new JTextField();
-		textSenha.setColumns(10);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
+		JButton btnSair = new JButton("Sair");
+		btnSair.setForeground(new Color(255, 255, 255));
+		btnSair.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnSair.setBackground(new Color(255, 0, 0));
+		btnSair.setBounds(1095, 573, 101, 42);
+		btnSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
-				Tela_Menu menu = new Tela_Menu();
-				menu.setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximize a tela
-				menu.setVisible(true);
 			}
 		});
+		contentPane.setLayout(null);
+		contentPane.add(btnSair);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(49, 83, 181));
+		panel.setBounds(310, 279, 492, 351);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setForeground(new Color(255, 255, 255));
+		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnLogin.setBackground(new Color(0, 0, 160));
+		btnLogin.setBounds(71, 240, 138, 60);
+		panel.add(btnLogin);
 		
 		JButton btnCadastrodUsuario = new JButton("Cadastre-se");
+		btnCadastrodUsuario.setForeground(new Color(255, 255, 255));
+		btnCadastrodUsuario.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnCadastrodUsuario.setBackground(new Color(0, 0, 160));
+		btnCadastrodUsuario.setBounds(291, 241, 138, 59);
+		panel.add(btnCadastrodUsuario);
+		
+		textEmail = new JTextField();
+		textEmail.setBackground(new Color(49, 83, 181));
+		textEmail.setBounds(167, 158, 234, 19);
+		panel.add(textEmail);
+		textEmail.setColumns(10);
+		
+		textSenha = new JTextField();
+		textSenha.setBackground(new Color(49, 83, 181));
+		textSenha.setBounds(167, 105, 234, 19);
+		panel.add(textSenha);
+		textSenha.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(56, 106, 68, 13);
+		panel.add(lblEmail);
+		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 15));
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblSenha.setBounds(56, 159, 68, 13);
+		panel.add(lblSenha);
 		btnCadastrodUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -91,54 +126,13 @@ public class Tela_Login extends JFrame {
 			
 			}
 		});
-		
-		JButton btnSair = new JButton("Sair");
-		btnSair.addActionListener(new ActionListener() {
+		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
+				Tela_Menu menu = new Tela_Menu();
+				menu.setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximize a tela
+				menu.setVisible(true);
 			}
 		});
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(194)
-					.addComponent(btnLogin)
-					.addGap(71)
-					.addComponent(btnCadastrodUsuario)
-					.addPreferredGap(ComponentPlacement.RELATED, 291, Short.MAX_VALUE)
-					.addComponent(btnSair)
-					.addGap(56))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(202)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(lblSenha, Alignment.TRAILING)
-						.addComponent(lblEmail, Alignment.TRAILING))
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(textEmail)
-						.addComponent(textSenha, GroupLayout.DEFAULT_SIZE, 125, Short.MAX_VALUE))
-					.addContainerGap(436, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(197)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblSenha)
-						.addComponent(textSenha, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(32)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblEmail)
-						.addComponent(textEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(55)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnLogin)
-						.addComponent(btnCadastrodUsuario)
-						.addComponent(btnSair))
-					.addGap(41))
-		);
-		contentPane.setLayout(gl_contentPane);
 	}
-
 }
