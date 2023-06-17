7b9cfcd858bae5f8ce7b29d34fa4decf15e72e49
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-06-17 10:42:12 -0300

    vamu eclipseee

--- src/visao/Tela_CadastrarPassageiro.java
+++ src/visao/Tela_CadastrarPassageiro.java
@@ -13,6 +13,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JComboBox;
 
 public class Tela_CadastrarPassageiro extends JFrame {
 
@@ -25,47 +28,107 @@ public class Tela_CadastrarPassageiro extends JFrame {
 	private JButton btnVoltar;
 	private JLabel lblQuatBag;
 	private JTextField txtQuantBag;
-	private JTextField txtPesoBagagem;
+	private JPanel panel;
+	private JLabel lblInformeOsDados;
 	
 	public Tela_CadastrarPassageiro() {
 		setTitle("Cadastro de Passageiros");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1177, 673);
+		setBounds(100, 100, 1254, 735);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(56, 124, 154));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNome = new JLabel("Nome completo:");
-		lblNome.setBounds(260, 289, 119, 14);
+		panel = new JPanel();
+		panel.setBackground(new Color(56, 124, 154));
+		panel.setBounds(148, 129, 1079, 508);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
 		textNome = new JTextField();
-		textNome.setBounds(383, 286, 665, 20);
+		textNome.setBackground(new Color(56, 124, 154));
+		textNome.setBounds(305, 119, 764, 20);
+		panel.add(textNome);
 		textNome.setColumns(10);
 		
-		JLabel lblEmail = new JLabel("Email:");
-		lblEmail.setBounds(281, 365, 81, 14);
-		
-		textEmail = new JTextField();
-		textEmail.setBounds(383, 362, 763, 20);
-		textEmail.setColumns(10);
-		
-		JLabel lblCpf = new JLabel("Cpf:");
-		lblCpf.setBounds(281, 414, 43, 14);
-		
-		textCpf = new JTextField();
-		textCpf.setBounds(383, 411, 763, 20);
-		textCpf.setColumns(10);
-		
-		JLabel lblTelefone = new JLabel("Telefone:");
-		lblTelefone.setBounds(281, 327, 135, 14);
-		
 		textTelefone = new JTextField();
-		textTelefone.setBounds(383, 324, 763, 20);
+		textTelefone.setBackground(new Color(56, 124, 154));
+		textTelefone.setBounds(306, 167, 763, 20);
+		panel.add(textTelefone);
 		textTelefone.setColumns(10);
 		
+		textEmail = new JTextField();
+		textEmail.setBackground(new Color(56, 124, 154));
+		textEmail.setBounds(306, 210, 763, 20);
+		panel.add(textEmail);
+		textEmail.setColumns(10);
+		
+		textCpf = new JTextField();
+		textCpf.setBackground(new Color(56, 124, 154));
+		textCpf.setBounds(306, 260, 763, 20);
+		panel.add(textCpf);
+		textCpf.setColumns(10);
+		
+		txtQuantBag = new JTextField();
+		txtQuantBag.setBackground(new Color(56, 124, 154));
+		txtQuantBag.setBounds(305, 372, 86, 20);
+		panel.add(txtQuantBag);
+		txtQuantBag.setColumns(10);
+		
+		btnVoltar = new JButton("Voltar");
+		btnVoltar.setBackground(new Color(255, 128, 128));
+		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnVoltar.setBounds(571, 402, 126, 47);
+		panel.add(btnVoltar);
+		
 		JButton btnSalvar = new JButton("Salvar");
-		btnSalvar.setBounds(383, 576, 81, 23);
+		btnSalvar.setBackground(new Color(0, 255, 128));
+		btnSalvar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnSalvar.setBounds(571, 323, 126, 47);
+		panel.add(btnSalvar);
+		
+		JLabel lblPesoBag = new JLabel("Peso bagagem:");
+		lblPesoBag.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblPesoBag.setBounds(162, 371, 133, 21);
+		panel.add(lblPesoBag);
+		
+		lblQuatBag = new JLabel("QNTD bagagem:");
+		lblQuatBag.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblQuatBag.setBounds(162, 321, 160, 21);
+		panel.add(lblQuatBag);
+		
+		JLabel lblCpf = new JLabel("Cpf:");
+		lblCpf.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblCpf.setBounds(236, 253, 43, 26);
+		panel.add(lblCpf);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblEmail.setBounds(225, 209, 81, 14);
+		panel.add(lblEmail);
+		
+		JLabel lblTelefone = new JLabel("Telefone:");
+		lblTelefone.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblTelefone.setBounds(196, 166, 135, 14);
+		panel.add(lblTelefone);
+		
+		JLabel lblNome = new JLabel("Nome completo:");
+		lblNome.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblNome.setBounds(151, 118, 144, 14);
+		panel.add(lblNome);
+		
+		lblInformeOsDados = new JLabel("INFORME OS DADOS DO PASSAGEIRO");
+		lblInformeOsDados.setFont(new Font("Tahoma", Font.BOLD, 32));
+		lblInformeOsDados.setBounds(22, 10, 825, 39);
+		panel.add(lblInformeOsDados);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBackground(new Color(56, 124, 154));
+		comboBox.setBounds(305, 321, 86, 21);
+		panel.add(comboBox);
 		btnSalvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -74,9 +137,6 @@ public class Tela_CadastrarPassageiro extends JFrame {
 				escolherAssento.setVisible(true);
 			}
 		});
-		
-		btnVoltar = new JButton("Voltar");
-		btnVoltar.setBounds(523, 576, 61, 23);
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -85,35 +145,6 @@ public class Tela_CadastrarPassageiro extends JFrame {
 				menu.setVisible(true);
 			}
 		});
-		
-		lblQuatBag = new JLabel("Quantidade de bagagem: ");
-		lblQuatBag.setBounds(219, 474, 160, 14);
-		
-		txtQuantBag = new JTextField();
-		txtQuantBag.setBounds(383, 471, 86, 20);
-		txtQuantBag.setColumns(10);
-		
-		JLabel lblPesoBag = new JLabel("Peso bagagem");
-		lblPesoBag.setBounds(260, 529, 102, 14);
-		
-		txtPesoBagagem = new JTextField();
-		txtPesoBagagem.setBounds(383, 526, 86, 20);
-		txtPesoBagagem.setColumns(10);
-		contentPane.setLayout(null);
-		contentPane.add(btnVoltar);
-		contentPane.add(lblTelefone);
-		contentPane.add(lblNome);
-		contentPane.add(lblCpf);
-		contentPane.add(lblEmail);
-		contentPane.add(lblQuatBag);
-		contentPane.add(textEmail);
-		contentPane.add(textTelefone);
-		contentPane.add(textNome);
-		contentPane.add(textCpf);
-		contentPane.add(txtQuantBag);
-		contentPane.add(lblPesoBag);
-		contentPane.add(txtPesoBagagem);
-		contentPane.add(btnSalvar);
 	}
 }
 
--- src/visao/Tela_CadastroDeVoo.java
+++ src/visao/Tela_CadastroDeVoo.java
@@ -11,6 +11,8 @@ import javax.swing.JComboBox;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Tela_CadastroDeVoo extends JFrame {
 
@@ -18,69 +20,110 @@ public class Tela_CadastroDeVoo extends JFrame {
 	private JTextField txtDestino;
 	private JTextField txtHorario;
 	private JTextField txtData;
+	private JTextField textField;
 
 	
 	public Tela_CadastroDeVoo() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1176, 663);
+		setBounds(100, 100, 1229, 728);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(56, 124, 154));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblCadastrarVoo = new JLabel("Cadastrar VOO");
-		lblCadastrarVoo.setBounds(10, 11, 206, 14);
-		contentPane.add(lblCadastrarVoo);
-		
-		JLabel lblDestino = new JLabel("Destino");
-		lblDestino.setBounds(22, 114, 206, 14);
-		contentPane.add(lblDestino);
-		
-		JLabel lblHorario = new JLabel("Horario:");
-		lblHorario.setBounds(22, 162, 206, 14);
-		contentPane.add(lblHorario);
-		
-		JLabel lblData = new JLabel("Data:");
-		lblData.setBounds(22, 207, 206, 14);
-		contentPane.add(lblData);
-		
-		txtDestino = new JTextField();
-		txtDestino.setBounds(163, 111, 254, 20);
-		contentPane.add(txtDestino);
-		txtDestino.setColumns(10);
-		
-		txtHorario = new JTextField();
-		txtHorario.setColumns(10);
-		txtHorario.setBounds(163, 159, 254, 20);
-		contentPane.add(txtHorario);
-		
-		txtData = new JTextField();
-		txtData.setColumns(10);
-		txtData.setBounds(163, 204, 254, 20);
-		contentPane.add(txtData);
-		
-		JLabel lblAvio = new JLabel("Avião:");
-		lblAvio.setBounds(421, 11, 129, 14);
-		contentPane.add(lblAvio);
-		
-		JComboBox cmbAviao = new JComboBox();
-		cmbAviao.setBounds(565, 7, 30, 22);
-		contentPane.add(cmbAviao);
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(56, 124, 154));
+		panel.setBounds(177, 124, 1010, 589);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
 		JButton btnVoltar = new JButton("Voltar");
-		btnVoltar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				Tela_Menu menu = new Tela_Menu();
-				menu.setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximize a tela
-				menu.setVisible(true);
-			}
-		});
-		btnVoltar.setBounds(484, 366, 89, 23);
-		contentPane.add(btnVoltar);
+		btnVoltar.setBackground(new Color(255, 128, 128));
+		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnVoltar.setBounds(652, 488, 144, 47);
+		panel.add(btnVoltar);
 		
 		JButton btnSalvar = new JButton("Salvar");
+		btnSalvar.setForeground(new Color(192, 192, 192));
+		btnSalvar.setBackground(new Color(0, 0, 255));
+		btnSalvar.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnSalvar.setBounds(386, 488, 144, 47);
+		panel.add(btnSalvar);
+		
+		txtData = new JTextField();
+		txtData.setBackground(new Color(56, 124, 154));
+		txtData.setBounds(457, 370, 322, 20);
+		panel.add(txtData);
+		txtData.setColumns(10);
+		
+		txtHorario = new JTextField();
+		txtHorario.setBackground(new Color(56, 124, 154));
+		txtHorario.setBounds(457, 228, 322, 20);
+		panel.add(txtHorario);
+		txtHorario.setColumns(10);
+		
+		txtDestino = new JTextField();
+		txtDestino.setBackground(new Color(56, 124, 154));
+		txtDestino.setBounds(457, 153, 322, 20);
+		panel.add(txtDestino);
+		txtDestino.setColumns(10);
+		
+		JLabel lblData = new JLabel("Data:");
+		lblData.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblData.setBounds(290, 372, 206, 14);
+		panel.add(lblData);
+		
+		JLabel lblHorario = new JLabel("Horario:");
+		lblHorario.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblHorario.setBounds(290, 227, 206, 14);
+		panel.add(lblHorario);
+		
+		JLabel lblDestino = new JLabel("Destino:");
+		lblDestino.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblDestino.setBounds(290, 155, 206, 14);
+		panel.add(lblDestino);
+		
+		JLabel lblCadastrarVoo = new JLabel("Cadastrar VOÔ");
+		lblCadastrarVoo.setFont(new Font("Tahoma", Font.BOLD, 32));
+		lblCadastrarVoo.setBounds(35, 27, 254, 39);
+		panel.add(lblCadastrarVoo);
+		
+		textField = new JTextField();
+		textField.setBackground(new Color(56, 124, 154));
+		textField.setColumns(10);
+		textField.setBounds(457, 300, 322, 20);
+		panel.add(textField);
+		
+		JLabel lblPreo = new JLabel("Valor($):");
+		lblPreo.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblPreo.setBounds(290, 303, 206, 14);
+		panel.add(lblPreo);
+		
+		JLabel lblPreo_1 = new JLabel("*");
+		lblPreo_1.setForeground(new Color(255, 0, 0));
+		lblPreo_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblPreo_1.setBounds(379, 370, 51, 14);
+		panel.add(lblPreo_1);
+		
+		JLabel lblPreo_1_1 = new JLabel("*");
+		lblPreo_1_1.setForeground(Color.RED);
+		lblPreo_1_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblPreo_1_1.setBounds(379, 156, 51, 14);
+		panel.add(lblPreo_1_1);
+		
+		JLabel lblPreo_1_2 = new JLabel("*");
+		lblPreo_1_2.setForeground(Color.RED);
+		lblPreo_1_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblPreo_1_2.setBounds(386, 231, 51, 14);
+		panel.add(lblPreo_1_2);
+		
+		JLabel lblPreo_1_3 = new JLabel("*");
+		lblPreo_1_3.setForeground(Color.RED);
+		lblPreo_1_3.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblPreo_1_3.setBounds(386, 303, 51, 14);
+		panel.add(lblPreo_1_3);
 		btnSalvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -89,7 +132,13 @@ public class Tela_CadastroDeVoo extends JFrame {
 				menu.setVisible(true);
 			}
 		});
-		btnSalvar.setBounds(139, 366, 89, 23);
-		contentPane.add(btnSalvar);
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				Tela_Menu menu = new Tela_Menu();
+				menu.setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximize a tela
+				menu.setVisible(true);
+			}
+		});
 	}
 }
