6b15929639e71d7af8db205c3dda23e5f16e18ec
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-06-17 11:25:58 -0300

    arruma dao

--- src/controle/UsuarioDAO.java
+++ src/controle/UsuarioDAO.java
@@ -1,38 +1,5 @@
 package controle;
 
-import java.util.ArrayList;
-
-import modelo.Usuario;
-
 public class UsuarioDAO {
 
-	private static ArrayList<Usuario> TUser;
-	
-	private static UsuarioDAO FUsuarioDAO;
-	
-	//Instacia
-	public static UsuarioDAO getFUsuarioDAO() {
-		if (FUsuarioDAO == null) {
-			FUsuarioDAO = new UsuarioDAO();
-			TUser    = new ArrayList<>();
-		}
-		return FUsuarioDAO;
-	}
-	
-	//Insert
-	public Boolean insert(Usuario prModel) {
-		if(prModel != null) {
-			this.TUser.add(prModel);
-			return true;
-		}
-		return false;
-	}
-	
-	
-	
-	//Select
-	public ArrayList<Usuario> getList(){
-		return TUser;
-	}
-	
 }
